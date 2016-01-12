8a11ba6cca9c307082e0864e246906704d0dc852
Ebony <esn05@yahoo.com>
2016-01-12 18:15:58 -0500

    Setup DetailVC UI, installed Alamofire, and added properties to Pokemon model class

--- Pokedex/Pokedex/Pokemon.swift
+++ Pokedex/Pokedex/Pokemon.swift
@@ -12,6 +12,13 @@ class Pokemon {
     
     private (set) var name: String!
     private (set) var pokedexId: Int!
+    private (set) var description: String!
+    private (set) var type: String!
+    private (set) var defense: String!
+    private (set) var height: String!
+    private (set) var weight: String!
+    private (set) var attack: String!
+    private (set) var nextEvolutionTxt: String!
     
     
     init(name: String, pokedexId: Int) {
--- Pokedex/Pokedex/PokemonDetailVC.swift
+++ Pokedex/Pokedex/PokemonDetailVC.swift
@@ -11,14 +11,40 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    
+    @IBOutlet weak var defenseLbl: UILabel!
+    
+    
+    @IBOutlet weak var heightLbl: UILabel!
+    
+    @IBOutlet weak var pokedexLbl: UILabel!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    
     var poke: Pokemon!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLbl.text = poke.name
 
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+  
+    
+    
 
 
     /*
